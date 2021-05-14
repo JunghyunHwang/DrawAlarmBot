734be9866c76f9928a81a102a88b606a3b176d7f
Ja_Hwang <dmagk560@gmail.com>
2021-05-14 13:20:08 +0900

    Add scraping library and get data list

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const mysql = require("mysql")
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const app = express();
+
+dotenv.config({path: './.env'});
 
 const db = mysql.createConnection ({
   host: process.env.DATABASE_HOST,
@@ -11,15 +15,45 @@ const db = mysql.createConnection ({
 });
 
 db.connect((error) => {
-  if(error) {
-    console.log(`${error} fuck~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
-  }
-  else {
-    console.log("Nice job!");
+  if (error) {
+    console.log(error);
   }
 });
 
-app.listen(4000, () =>
-{
-    console.log("Server is running like a Ninja");
+const getHtml = async () => {
+  try {
+    return await axios.get("https://www.nike.com/kr/launch/?type=upcoming&activeDate=date-filter:AFTER_DATE");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml()
+  .then(html => {
+    let ulList = [];
+    let drawList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("ul.gallery li");
+
+    $bodyList.each(function (i, elem) {
+      ulList[i] = {
+        title: $(this).find('div.ta-sm-c a').text(),
+        url: $(this).find('a.comingsoon').attr('href')
+      }; 
+    });
+    console.log(ulList);
+
+    /*
+    let cnt = 0;
+    for (let i = 0; i < ulList.length; i++) {
+      if (ulList[i].title == "THE DRAW 진행예정") {
+        cnt++;
+      }
+    }
+    console.log(cnt);
+    */
+  });
+
+app.listen(4000, () => {
+  console.log("Server is running like a Ninja");
 });
