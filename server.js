7d45ce609d53b989ed60b49ba9e812a0b0137a6f
Ja_Hwang <dmagk560@gmail.com>
2021-10-13 22:53:35 +0900

    Add logging module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
+const logging = require('./log.js');
 
 const app = express();
 
