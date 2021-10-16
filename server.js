05e76dc9983785346ad6504346fea4d34100f069
junghyunhwang <dmagk560@gmail.com>
2021-10-16 19:06:41 +0900

    Add error handle

--- server.js
+++ server.js
@@ -14,12 +14,33 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-db.connect((error) => {
-    if (error) {
-        logging('error', "DB disconnected");
-        console.log("Fuck Crash");
-    }
-});
+// db.connect((error) => {
+//     if (error) {
+//         logging('error', "DB disconnected");
+//         console.log("Fuck Crash");
+//     }
+// });
+
+function handleDisconnect() {
+    db.connect(function(err) {            
+        if(err) {                            
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }                                   
+    });                                 
+                                            
+    db.on('error', function(err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
+            return handleDisconnect();                      
+        } 
+        else {
+            throw err;          
+        }
+    });
+}
+
+handleDisconnect();
 
 app.use('/', require('./routes/controller'));
 
