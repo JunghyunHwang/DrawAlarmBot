2f26c97478b4831a20f2d571e8c222fb1d314baf
junghyunhwang <dmagk560@gmail.com>
2021-10-16 19:19:31 +0900

    Remove db connection

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ const app = express();
 
 dotenv.config();
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
-
-db.connect((error) => {
-    if (error) {
-        logging('error', "DB disconnected");
-    }
-});
-
 app.use('/', require('./routes/controller'));
 
 app.listen(port, () => {
