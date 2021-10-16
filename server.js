87e354d17cfe1a396c7d4c452bb815236b05832c
Ja_Hwang <dmagk560@gmail.com>
2021-10-17 00:44:31 +0900

    Add db connect test code

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const dotenv = require('dotenv');
 const logging = require('./log.js');
 const port = process.env.PORT || 3000;
 const app = express();
+const db = require('./db.js');
 
 dotenv.config();
+
+db.query('SELECT * FROM draw_info', (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(row);
+    }
+});
 
 app.use('/', require('./routes/controller'));
 
