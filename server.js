f96fd52a71d0ece308352ca5645dc03aa9b16f7a
Ja_Hwang <dmagk560@gmail.com>
2021-10-17 01:32:35 +0900

    Remove error handle

--- server.js
+++ server.js
@@ -4,29 +4,8 @@ const dotenv = require('dotenv');
 const logging = require('./log.js');
 const port = process.env.PORT || 3000;
 const app = express();
-const db = require('./db.js');
 
 dotenv.config();
-
-function handleDisconnect() {
-    db.connect(function(err) {
-        if(err) {
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-    db.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } 
-        else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect();
 
 app.use('/', require('./routes/controller'));
 
